f1173cfed6fc08e27ac6fb6683a8966186d200e8
haoyjp <haoyjp@163.com>
2018-03-17 13:20:24 +0000

    Finish basic page layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,inital-scale=1">
+    <!--CSRF Token-->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title','LaraBBS') -Laravel进阶教程</title>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app" class="{{ route_class() }}-page">
+        @include('layouts._header')
+        <div class="container">
+            @yield('content')
+        </div>
+
+        @include('layouts._footer')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('ja/app.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,5 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/','PagesController@root')->name('root');
 
